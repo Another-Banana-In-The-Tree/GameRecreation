ce574dcf170c12a190ae38be18140a07979566a1
JaydenWiz <Jayden.Wisniewski@OntarioTechU.net>
2023-11-09 16:41:09 -0500

    Mario animated

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -1,14 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    //_-_-_-ANIMATION-_-_-_
+    public Animator animator;
+    //_-_-_-_-_-_-_-_-_-_-_
+
+
     //Vars for speed and jumping
     [Header("Movement and Jumping Force")]
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private Vector2 Movement; //checks movement for animations
+    SpriteRenderer sprite;
     
     //Vars for checking grounded
     private bool _isGrounded;
@@ -26,6 +35,7 @@ public class Player : MonoBehaviour
     private void Start()
     {
         InputManager.Init(this);
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -34,6 +44,33 @@ public class Player : MonoBehaviour
         CheckGround();
         _rb = GetComponent<Rigidbody2D>();
         _depth = GetComponent<BoxCollider2D>().bounds.size.y;
+
+
+        //_-_-_-_-_-JANK ANIMATION MATH :)-_-_-_-_-_
+        Movement = speed * Time.deltaTime * _moveDir;
+        float Movef = (float)Movement.x;
+        animator.SetFloat("Speed", Mathf.Abs(Movef));
+
+        if (Movement.x > 0) 
+        {
+            sprite.flipX = false;
+        }
+
+        else if (Movement.x < 0) 
+        {
+            sprite.flipX = true;
+        }
+
+        if (_isGrounded == true)  
+        {
+            animator.SetBool("IsJumping", false);
+        }
+        else
+        {
+            animator.SetBool("IsJumping", true);
+        }
+        //_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
     }
     
     public void SetMovementDirection(Vector2 newDirection)
@@ -48,6 +85,7 @@ public class Player : MonoBehaviour
             Debug.Log("Jump called");
             _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
         }
+
     }
     
     private void CheckGround()
