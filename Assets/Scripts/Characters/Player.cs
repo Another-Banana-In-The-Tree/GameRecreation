706cf6c07bad08de5f0291e7511680c8d3e134c5
sltz342 <133233836+sltz342@users.noreply.github.com>
2023-11-20 09:40:45 -0500

    updating palyer
    **

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -10,6 +10,9 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
     
+    
+    private float currentVelocity = 0;
+    
     //Vars for checking grounded
     private bool _isGrounded;
     private Rigidbody2D _rb;
@@ -30,12 +33,60 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+<<<<<<< Updated upstream
         transform.position += transform.rotation * (speed * Time.deltaTime * _moveDir);
+=======
+        animator.playbackTime = Time.deltaTime;
+        
+        transform.position += transform.rotation * (currentVelocity  * Time.deltaTime * _moveDir);
+>>>>>>> Stashed changes
         CheckGround();
+        
         _rb = GetComponent<Rigidbody2D>();
         _depth = GetComponent<BoxCollider2D>().bounds.size.y;
     }
+<<<<<<< Updated upstream
     
+=======
+
+    public void FixedUpdate()
+    {
+        Movement = speed * Time.deltaTime * _moveDir;
+        if (currentVelocity <= speed && Movement.x > 0)
+        {
+            currentVelocity+= 0.1f;
+            Debug.Log("current speed at " + currentVelocity);
+        }
+        else
+        {
+            currentVelocity = 0;
+        }
+        //_-_-_-_-_-JANK ANIMATION MATH :)-_-_-_-_-_
+        
+        float Movef = (float)Movement.x;
+        animator.SetFloat("Speed", Mathf.Abs(Movef));
+
+        if (Movement.x > 0)
+        {
+            sprite.flipX = false;
+        }
+
+        else if (Movement.x < 0)
+        {
+            sprite.flipX = true;
+        }
+
+        if (_isGrounded == true)
+        {
+            animator.SetBool("IsJumping", false);
+        }
+        else
+        {
+            animator.SetBool("IsJumping", true);
+        }
+        //_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+    }
+>>>>>>> Stashed changes
     public void SetMovementDirection(Vector2 newDirection)
     {
         _moveDir = newDirection;
