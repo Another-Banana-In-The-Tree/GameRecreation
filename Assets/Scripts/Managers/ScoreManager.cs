86dd7d8a9e6019aef208b30fb112b49ce5bbfd50
DwaynededwedwibbleXS <david.bacon@ontariotechu.net>
2023-12-03 14:53:52 -0500

    Added UI

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    [SerializeField] private int score = 0;
+    private void Awake()
+    {
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+
+        }
+
+        Debug.Log(score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore()
+    {
+        score++;
+        UiManager.instance.NanasText(score);
+    }
+
+    
+}
--- Assets/Scripts/Managers/UiManager.cs
+++ Assets/Scripts/Managers/UiManager.cs
@@ -5,17 +5,54 @@ using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
-    [SerializeField] GameManager gameManager;
+    public static UiManager instance;
     [SerializeField] private Text _text;
-    void Start()
+    [SerializeField] private Text _nanaText;
+
+    private void Start()
+    {
+        _text.text = "Lives x " + GameManager.instance.GetCurrentLives().ToString();
+
+        _nanaText.text = "Score x 0";
+    }
+
+    private void Awake()
     {
         
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+
+        
+
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        //work in progress
-        //_text.text = "Lives x" + lives.ToString();
+        
+        
+        
     }
+
+    public void LivesText(int lives)
+    {
+        _text.text = "Lives x " + lives.ToString();
+    }
+
+    public void NanasText(int score)
+    {
+        _nanaText.text = "Score x " + score.ToString();
+
+    }
+
+
 }
