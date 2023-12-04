4be5c20bd71f21f0408385ffb5ca7e8b02fee6c0
Ryan <ryan.dean@ontariotechu.net>
2023-12-04 01:22:29 -0500

    Fixed Secret Holes

--- Assets/Scripts/Managers/SecretManager.cs
+++ Assets/Scripts/Managers/SecretManager.cs
@@ -6,22 +6,19 @@ using UnityEngine;
 public class SecretManager : MonoBehaviour
 {
     //_-_-Funky lil' variables_-_-_
-    GameObject player;
+    Player player;
     public Transform teleportLocation;
     //_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
     //Get player using Player tag
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        player = GameObject.FindObjectOfType<Player>();
     }
     //When the player collides with the collider, teleport them to the new location
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag != "Fireball")
-        {
-            FindObjectOfType<AudioManager>().Play("Pipe");
-            player.transform.position = teleportLocation.position;
-        }
+        FindObjectOfType<AudioManager>().Play("Pipe");
+        player.transform.position = teleportLocation.position;
     }
 }
